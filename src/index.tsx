b95122859bf7bba56a80d6e9a5861a67109099e0
Zakharii-Husar <zakhariihusar@gmail.com>
2023-08-26 23:29:01 -0500

    Changed code to TS

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,22 @@ import { createRoot } from 'react-dom/client'
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 
-function Box(props: any) {
+interface BoxProps {
+  position: [number, number, number]
+}
+
+function Box(props: BoxProps) {
   // This reference will give us direct access to the mesh
-  const meshRef = useRef()
+  const meshRef = useRef<THREE.Mesh>(null)
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+  useFrame((state, delta) => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += delta
+    }
+  })
   // Return view, these are regular three.js elements expressed in JSX
   return (
     <mesh
@@ -20,16 +28,19 @@ function Box(props: any) {
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <meshStandardMaterial color={hovered ? 'red' : 'green'} />
     </mesh>
   )
 }
 
-createRoot(document.getElementById('root')).render(
-  <Canvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    <Box position={[-1.2, 0, 0]} />
-    <Box position={[1.2, 0, 0]} />
-  </Canvas>,
-)
+const rootElement = document.getElementById('root')
+if (rootElement) {
+  createRoot(rootElement).render(
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
+}
